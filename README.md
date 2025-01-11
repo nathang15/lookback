81ab80a4e8e21bd0a59ae2d5fc350dbfa731c029
Nathan Nguyen <nhanngyn15@gmail.com>
2025-01-11 18:13:11 +0700

    update load_data

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Get query from the user
+2. Retrieve data from yfinance
+3. Match up the query with the correct strategies
+4. Execute the strategies
+5. Return the result (total returns) and plot
+
+### Note
+- We exclude microcaps because they are so volatile that it's not guaranteed to provide a good analysis
--- utils/load_data.py
+++ utils/load_data.py
@@ -1,14 +1,24 @@
 import pandas as pd
-from type_convert import convert_data
+from utils.type_convert import convert_data
 
 class LoadData:
     def __init__(self, prompt: str):
         # convert data from csv to dataframe
-        self._tickers = pd.read_csv("../data/all_ticker_data.csv", header=0).transpose()
+        self._tickers = pd.read_csv("./data/all_ticker_data.csv", header=0, low_memory=False).transpose()
+        
+        # Store column names from first row
         self._tickers_columns = self._tickers.iloc[0]
+        
+        # Remove the first row
         self._tickers = self._tickers[1:]
         self._tickers.reset_index(inplace=True)
         self._tickers.rename(columns={"index": "ticker"}, inplace=True)
+        
+        # Create and apply column mapping
+        column_mapping = {i: name for i, name in enumerate(self._tickers_columns)}
+        self._tickers = self._tickers.rename(columns=column_mapping)
+        
+        # Convert data types
         self._tickers = convert_data(self._tickers)
 
     @property
